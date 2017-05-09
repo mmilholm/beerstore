f66da7dbbb305b026361ebac7b25538c7c987195
Georgi Angelov <geoa610@gmail.com>
2017-05-08 23:13:55 -0700

    debug database script and populate script

--- db/b-populate.sql
+++ db/b-populate.sql
@@ -6,9 +6,9 @@ VALUES
 	('Stout');
 INSERT INTO tblProducts
 	(prod_name,
-  product_description,
+  prod_description,
   prod_picture,
-  product_price,
+  prod_price,
   cat_id)
   VALUES('Blue Buck','good beer', '/dbImages/beer_ale_bluebuck.png',11.99,1 ),
 		('Cypress Honey Lager','another good beer', '/dbImages/beer_lager_cypresshoney.png ',13.99,2 ),

ee01b105e93e5a81ee24577ee63e96d8008c931a
robi199 <joseph.robillard@online.camosun.ca>
2017-05-17 15:15:32 -0700

    Added beer page

---
+++ src/beer.php
@@ -0,0 +1,206 @@
+<?php
+session_start();
+require_once "model/db_functions.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+	<link rel="stylesheet" type="text/css" href="css/basecss.css">
+
+	<title>the beer shoppe - Camosun ICS Year One Project</title>
+
+	<!-- Bootstrap Core CSS -->
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+<div id="header_div">
+	<div id="login_div">
+		<?php
+
+		$current_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+		if (isset ($_SESSION['user']) && isset($_SESSION['user']['user_id']))
+		{
+			echo 'Hello, ' . $_SESSION['user']['first_name']. ' ' .$_SESSION['user']['last_name']. ' <a href="logout.php?origin=' . $current_url . '">Log out</a>';
+		}
+		else
+		{
+			echo '<a href="login.php?origin='. $current_url . '"> Hello, Sign in </a>';
+		}
+		?>
+	   <a href=""> Cart(#) </a>
+    </div>
+
+	<div id="menubar">
+    <a href="">Home</a>  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="beer.php">Beer</a>  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="merch.php">Gifts</a>  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+    <a href="contact.php">Contact</a>  &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+
+
+    </div>
+
+</div>
+
+
+<div class="container text-center">
+	<div class="row">
+		<div class="col-sm-3 well">
+			<!-- could put a search form here is wanted -->
+			    <form action="" method="post">
+					<button class="btn btn-info" name = "seshReset" type="submit" value="1"> Reset Session </button>
+				  </form>
+          <form action="" method="post">
+          <button class="btn btn-info" name = "itemType" type="submit" value="1"> Ale </button>
+          </form>
+          <form action="" method="post">
+          <button class="btn btn-info" name = "itemType" type="submit" value="2"> Lager </button>
+          </form>
+          <form action="" method="post">
+          <button class="btn btn-info" name = "itemType" type="submit" value="3"> Stout </button>
+          </form>
+          <form action="" method="post">
+          <button class="btn btn-info" name = "itemType" type="submit" value="4"> IPA </button>
+          </form>
+
+		</div>
+
+
+
+
+		<div class="col-sm-9">
+		<?php
+      if (isset($_POST['itemType'])){
+			  $count = 0;
+    		$names = getProdInfo($_POST['itemType']);
+    		foreach ($names as $name) {
+    			if ($count % 3 == 0) echo '<div class="row">';
+		?>
+				<div class="col-sm-4">
+					<div class="well">
+						<form action = "" method = "post">
+    						<fieldset>
+                				<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#<?php echo $name['prod_id']; ?>">
+
+                				<?php echo $name['company_name'] . "<br>" . $name['prod_name']; ?> <br><br>
+                				<img class="img-thumbnail" style = "height:200px; width:200px;" src="<?php echo $name[prod_picture]; ?>"> <br><br> <?php if ($name['prod_package'] != null) { echo $name['prod_package'] . " --- ";} echo "$" . $name['prod_price']; ?></p>
+
+                				</a>
+
+                				<div id="<?php echo $name['prod_id']; ?>" class="collapse">
+                					<?php echo $name['prod_description'] . "<br><br>" ?>
+                					Quantity:
+                					<input type="number" name="quantity" min="1" max="10">
+                					<?php require 'model/addButton.php'; ?>
+            					</div>
+            				</fieldset>
+        				</form>
+        			</div>
+				</div>
+
+		<?php
+			$count++;
+			if ($count % 3 == 0) echo '</div>';
+			}
+		?>
+    <?php
+    }
+    ?>
+		</div>
+
+	</div>
+</div>
+
+        <?php
+            $getUserID = 0;
+
+              if (isset($_POST['seshReset'])){
+                session_unset();
+                exit();
+              }
+
+        	//print_r($_SESSION['user']);
+
+            	if (isset($_POST['prod_id'])) {
+                	$quantity = filter_input(INPUT_POST, 'quantity', FILTER_SANITIZE_SPECIAL_CHARS);
+	            	$product = filter_input(INPUT_POST, 'prod_id', FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+                	if (!isset($_SESSION['cart'])) {
+                    	$_SESSION['cart'] = array ();
+                	}
+					//If an order has already been created, add the product to the order
+                    //if (isset($_SESSION['order_created'])){
+                      // Get the order id
+                      //$ordID = getOrderID($_SESSION['user']['user_id']);
+                      // Add item call here
+                    //}
+
+                    //If an order has not been created yet, create one and add the item to the order
+                    if (!isset($_SESSION['order_created'])){
+                      $_SESSION['order_created'] = true;
+
+                      if(isset($_SESSION['user']['user_id'])){
+                        $getUserID = $_SESSION['user']['user_id'];
+                        createOrder($getUserID);
+                        //Get order ID
+                        //$ordID = getOrderID($_SESSION['user']['user_id']);
+                        //Add item call here
+                      }
+                    }
+ 	            	if (array_key_exists($product, $_SESSION['cart']))
+ 	            	{
+ 		            	$quantity += $_SESSION['cart'][$product];
+ 		            	$_SESSION['cart'][$product] = $quantity;
+ 	            	}
+ 	            	else
+ 	            	{
+ 		            	$_SESSION['cart'][$product] = $quantity;
+ 	            	}
+
+
+ 	            	foreach($_SESSION['cart'] as $key => $value) {
+	                	echo "The product id is $key <br>";
+	                	echo "The quantity is $value <br><hr>";
+	            	}
+	        	}
+
+        ?>
+
+
+
+<div id="footer_div">
+footer
+</div>
+
+
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>

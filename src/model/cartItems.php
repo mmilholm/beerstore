4e39be0fddc8d1d60110bd05dcf5993271b884d9
Georgi Angelov <georgi_angelov@zoho.com>
2017-05-25 12:55:04 -0700

    Revert "Merge branch 'georgi' into 'master'"
    This reverts merge request !14

--- src/model/cartItems.php
+++ src/model/cartItems.php
@@ -1,20 +1,21 @@
 <?php
 
-function cartItems ( $product, $addQuantity) {
+function cartItems ($quantity, $product, $price) {
+
+
 
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = array ();
 
     }
 
- 	if (array_key_exists($product['prod_id'], $_SESSION['cart'])) {
+ 	if (array_key_exists($product, $_SESSION['cart'])) {
         //zero is the location of quantity in the array
- 		
- 		$_SESSION['cart'][$product['prod_id']]['quantity'] += $addQuantity;
- 	
+ 		$quantity += $_SESSION['cart'][$product][0];
+ 		$_SESSION['cart'][$product][0] = $quantity;
+ 		$_SESSION['cart'][$product][1] = ($price * $quantity);
  	} else {
- 		$_SESSION['cart'][$product['prod_id']] = $product;
- 		$_SESSION['cart'][$product['prod_id']]['quantity'] = $addQuantity;
+ 		$_SESSION['cart'][$product] = array($quantity, ($price * $quantity));
  	}
 }
 
@@ -30,21 +31,9 @@ function getNumberItems() {
 			$numItems += (int) $_SESSION['cart'][$key][0];
 	}
 	return $numItems;
-}
- 
-function printCartItem($item){
-$productRow = <<<DELIMITER
-<div class="row">
-<div> 
-<img  class="img-thumbnail" style = "height:100px; width:100px; float:left" src="{$item['prod_picture']}">                         	
-<span class="quantity">{$item['quantity']}</span>
-<span class="itemName">{$item['prod_name']}</span>
-<span class="popbtn"><a class="arrow"></a></span>
-<span class="price">{$item['prod_price']}</span>								                         
-</div>
-</div>
-DELIMITER;
-echo $productRow;
+
+
+
 }
 
 

0f0b01818f63f7657fdad447102f9c3d8b64d415
Georgi Angelov <geoa610@gmail.com>
2017-06-07 16:07:49 -0700

    database and homepage

--- src/index3.php
+++ src/index3.php
@@ -1,9 +1,7 @@
-
 <?php
 session_start();
 require_once "model/db_functions.php";
 require "model/cartItems.php";
-
 ?>
 
 <!DOCTYPE html>
@@ -14,121 +12,22 @@ require "model/cartItems.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
 	<title>the beer shoppe - Camosun ICS Year One Project</title>
 
-
-	<!-- General CSS scripts
-	<link rel="stylesheet" type="text/css" href="css/basecss.css">  -->
-
-	<!-- Bootstrap Core CSS -->
-    <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- Custom CSS -->
-    <link href="../css/shop-homepage.css" rel="stylesheet">
+  <link href="../css/shop-homepage.css" rel="stylesheet">
 
-    <!-- For shopping_cart.js -->
-    <script data-require="jquery" data-semver="2.1.4" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-    <script src="../shopping_cart.js"></script>
-
-
-    <!-- Link for font -->
-    <link href="https://fonts.googleapis.com/css?family=Share" rel="stylesheet">
-
-	<style>
-
-		body {
-			font-family: 'Share', cursive;
-		}
-
-		.navbar-right > li > a {
-  			padding-top: 0px;
-  			padding-bottom: 0px;
- 		}
-
-
-		.navbar {
- 			background: aqua;
-   			position: relative;
-    		margin-bottom: 0;
-    		min-height:28px;
-		}
-
-
-	</style>
 </head>
-
+<body style="overflow:hidden;">
 <!-- The stript for the header -->
-<?php require 'view/header.php'; ?>
-
-<div id="container-fluid" style="border: 1px solid black;height: 500px;"> <!-- Center of page between header and footer -->
-<div id ="row">
-<div class="col-md-3" style="border: 1px solid red;">Here</div>
-<div class="col-md-6" style="border: 1px solid red;">
-  <div class="row carousel-holder">
-
-      <div class="col-md-12">
-          <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-              <ol class="carousel-indicators">
-                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-              </ol>
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-                <div class="item">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-                <div class="item">
-                    <img class="slide-image" src="http://placehold.it/800x300" alt="">
-                </div>
-            </div>
-            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
-          </div>
-      </div>
-
-  </div>
-</div>
-<div class="col-md-3" style="border: 1px solid red;">BLAHHH</div>
-</div> <!-- end of row -->
-
-
-</div> <!-- end of center div -->
-
-
-
-
-
-
-
-
-
-<div id="footer_div" style="border: 1px solid black;">
-footer
-</div>
-
-
-
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <?php require 'background_video.php'; ?>
+  <?php require 'view/header.php'; ?>
 
 	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+     
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="../shopping_cart.js"></script>
 </body>
 </html>
